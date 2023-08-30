d4138666f594934efa989bfdacb4adc7475cdf7f
romariofutbolist <66all@mail.ru>
2023-08-30 14:44:37 +0300

    базовая сложность

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,64 @@
-public class Employee {
-    private String name;
-    private String department;
-    private int salary;
+import java.util.Objects;
 
+public class Employee {
+    private final String name;
+    private int department;
+    private double salary;
+    private int id;
+
+    static int COUNT;
+
+    public Employee(String name, int department, double salary) {
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+        this.id = COUNT++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return department == employee.department && Double.compare(employee.salary, salary) == 0 && id == employee.id && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, department, salary, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,69 @@
 public class Main {
 
+    private static final Employee[] employee = new Employee[10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        employee[0] = new Employee("Ivanov Ivan", 1, 60000.00);
+        employee[1] = new Employee("Ivanov Sergey", 2, 78000.00);
+        employee[2] = new Employee("Samokhina Alena", 1, 43000.00);
+        employee[3] = new Employee("Sidorov Peter", 3, 100000.00);
+        employee[4] = new Employee("Grigoriev Matvei", 3, 102000.00);
+        employee[5] = new Employee("Pizhov Leonid", 4, 45000.00);
+        employee[6] = new Employee("Artemev Roman", 5, 89000.00);
+        employee[7] = new Employee("Nikolaev Alex", 4, 38000.00);
+        employee[8] = new Employee("Kirillov Andrei", 2, 77000.00);
+        employee[9] = new Employee("Dimova Elena", 5, 79000.00);
+
+        printInfo();
+        System.out.println(sum());
+        System.out.println(min());
+        System.out.println(max());
+        System.out.println(average());
+        printOnlyName();
+
+    }
+
+    public static void printInfo() {
+        for (Employee value : employee) {
+            System.out.println(value);
+        }
+    }
+
+    public static double sum() {
+        double sum = 0.00;
+        for (Employee value : employee) {
+            sum += value.getSalary();
+        }
+        return sum;
+    }
+
+    public static Employee min() {
+        Employee minEmployee = employee[0];
+        for (Employee employee : employee) {
+            if (employee.getSalary() < minEmployee.getSalary()) {
+                minEmployee = employee;
+            }
+        }
+        return minEmployee;
+    }
+
+    public static Employee max() {
+        Employee maxEmployee = employee[0];
+        for (Employee employee : employee) {
+            if (employee.getSalary() > maxEmployee.getSalary()) {
+                maxEmployee = employee;
+            }
+        }
+        return maxEmployee;
+    }
+
+    public static double average() {
+        return sum() / employee.length;
+    }
+
+    public static void printOnlyName() {
+        for (Employee value : employee) {
+            System.out.println(value.getName());
+        }
     }
 }
